91ed1900178fae3d5b9806d37e6baf0a38730a1a
Ian Wohlgemuth <ianwohlgemuth@gmail.com>
2021-04-06 17:33:55 -0300

    added checkDeckCommand file and function on deckService

--- src/services/deckService.js
+++ src/services/deckService.js
@@ -19,14 +19,65 @@ class DeckService {
     await sheet.loadCells("H4:H520");
     const cells = [];
     for (let i = 4; i < 100; i++) {
-      cells.push(sheet.getCellByA1(`H${i}`));
+      //cells.push(sheet.getCellByA1(`H${i}`));
+      if (!sheet.getCellByA1(`H${i}`)._rawData.formattedValue) {
+        console.log(i-1);
+        return i;
+      }
     }
-    console.log(sheet.cellStats);
-    await sheet
+    //console.log(sheet.cellStats); why do this?
+    await sheet; //???
+  };
+
+  checkDecks = async () => {
+    const sheet = this.doc.sheetsByIndex[0];
+
+    await sheet.loadCells("O4:O80");
+    await sheet.loadCells("X4:X80");
+    //console.log("cells loaded correctly");
+
+    //SHOWS DECKS IN an array in discordMessage with their respective number and meta%
+    let discordMessage = "";
+
+    for (let i = 4; i < 14; i++) {
+      const currentDeckCell = sheet.getCellByA1(`O${i}`)._rawData
+        .formattedValue;
+      const currentDeckPercentage = sheet.getCellByA1(`X${i}`)._rawData
+        .formattedValue;
+      if (!currentDeckCell) {
+        break;
+      }
+
+      discordMessage =
+        discordMessage +
+        (i -
+          3 +
+          "." +
+          " " +
+          currentDeckPercentage +
+          " " +
+          currentDeckCell +
+          "\n");
+    }
+
+    //console.log(discordMessage);
+    return discordMessage;
+    /*
+    SHOWS DECKS IN decks ARRAY
+    const decks = [];
+    for (let i = 4; i < 20; i++) {
+      const currentDeckCell = sheet.getCellByA1(`O${i}`)._rawData
+        .formattedValue;
+
+      if (!currentDeckCell) {
+        break;
+      }
+
+      decks.push(currentDeckCell);
+    }
+    console.log(decks); */
   };
 }
-
 const deckService = new DeckService(Config.spreadsheet);
 deckService.initialize();
-
 module.exports = deckService;
