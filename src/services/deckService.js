9267deb00fa61bd0185c66bb165b4f04796984fc
Ian Wohlgemuth <ianwohlgemuth@gmail.com>
2021-05-12 18:57:21 -0300

    Improved addDeck performance

--- src/services/deckService.js
+++ src/services/deckService.js
@@ -3,17 +3,22 @@ const JsonStorageService = require("./jsonStorageService");
 const Config = require("../config/config.js");
 
 class DeckService {
+
   constructor(spreadsheetConfig) {
     this.doc = new GoogleSpreadsheet(spreadsheetConfig.spreadsheet_id);
     const { client_email, private_key } = spreadsheetConfig;
     this.creds = { client_email, private_key };
     this.jsonDataService = new JsonStorageService("deck-names.json");
+    this.workingSheet = null;
   }
 
   initialize = async () => {
     await this.doc.useServiceAccountAuth(this.creds);
     await this.doc.loadInfo();
     await this.jsonDataService.initialize();
+    this.workingSheet = this.doc.sheetsByIndex[0];
+    await this.workingSheet.loadCells("B80:C140");
+    await this.workingSheet.loadCells("H4:H520");
   };
 
   addDeck = async (deckName) => {
@@ -23,11 +28,9 @@ class DeckService {
       this.jsonDataService.saveObject({id: idOfDeck, displayName: deckName});
       deckDisplayName = deckName;
     }
-
-    const sheet = this.doc.sheetsByIndex[0];
-    const emptyCell = await this._findFirstEmptyCellInColumn(sheet, "H", 4, 520);
+    const emptyCell = await this._findFirstEmptyCellInColumn(this.workingSheet, "H", 4, 520);
     emptyCell.value = deckDisplayName;
-    await sheet.saveUpdatedCells();
+    await this.workingSheet.saveUpdatedCells();
     return deckDisplayName;
   };
 
@@ -36,7 +39,6 @@ class DeckService {
   }
 
   _findFirstEmptyCellInColumn = async (sheet, columnLetter, startRow, endRow) => {
-    await sheet.loadCells(`${columnLetter}${startRow}:${columnLetter}${endRow}`);
     for (let i = startRow; i <= endRow; i++) {
       const cell = sheet.getCellByA1(`${columnLetter}${i}`);
       if (!cell.value || cell.value === "") {
@@ -47,7 +49,6 @@ class DeckService {
   };
 
   _getColumnCells = async (sheet, columnLetter, startRow, endRow) => {
-    await sheet.loadCells(`${columnLetter}${startRow}:${columnLetter}${endRow}`);
     const cells = [];
     for (let i = startRow; i <= endRow; i++) {
       cells.push(sheet.getCellByA1(`${columnLetter}${i}`));
