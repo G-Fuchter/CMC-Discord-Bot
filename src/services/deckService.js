3e4efb5d3f8f780a7a0e96e6f522b1d44daa54bd
Ian Wohlgemuth <ianwohlgemuth@gmail.com>
2021-05-12 20:11:14 -0300

    Replaced JSON service with spreadsheet

--- src/services/deckService.js
+++ src/services/deckService.js
@@ -5,11 +5,14 @@ const Config = require("../config/config.js");
 class DeckService {
 
   constructor(spreadsheetConfig) {
+    const deckNameTable = { startRow: 80, endRow: 140 }
     this.doc = new GoogleSpreadsheet(spreadsheetConfig.spreadsheet_id);
     const { client_email, private_key } = spreadsheetConfig;
     this.creds = { client_email, private_key };
     this.jsonDataService = new JsonStorageService("deck-names.json");
     this.workingSheet = null;
+    this.deckNameTable = { startRow: 80, endRow: 180, startColumn:"B", endColumn:"C" }
+    //matchLogTable
   }
 
   initialize = async () => {
@@ -17,15 +20,15 @@ class DeckService {
     await this.doc.loadInfo();
     await this.jsonDataService.initialize();
     this.workingSheet = this.doc.sheetsByIndex[0];
-    await this.workingSheet.loadCells("B80:C140");
+    await this.workingSheet.loadCells("B80:C180");
     await this.workingSheet.loadCells("H4:H520");
   };
 
   addDeck = async (deckName) => {
     const idOfDeck = this._getIdOfDeck(deckName);
-    let deckDisplayName = this.jsonDataService.getObjectWithId(idOfDeck)?.displayName ?? null;
+    let deckDisplayName = this._getNameOfDeck(this.workingSheet, this.deckNameTable, idOfDeck);
     if (!deckDisplayName) {
-      this.jsonDataService.saveObject({id: idOfDeck, displayName: deckName});
+      await this._saveNameOfDeck(this.workingSheet, this.deckNameTable, idOfDeck, deckName);
       deckDisplayName = deckName;
     }
     const emptyCell = await this._findFirstEmptyCellInColumn(this.workingSheet, "H", 4, 520);
@@ -34,10 +37,31 @@ class DeckService {
     return deckDisplayName;
   };
 
-  _getIdOfDeck = (deckName) => {
-    return deckName.toLowerCase().split(" ").join("");
+  _getIdOfDeck = (deckNameInput) => {
+    return deckNameInput.toLowerCase().split(" ").join("");
   }
 
+  _getNameOfDeck = (sheet, deckNameTable, deckId) => {
+    for (let i = deckNameTable.startRow; i <= deckNameTable.endRow; i++) {
+      const cell = sheet.getCellByA1(`${deckNameTable.startColumn}${i}`);
+      if (!cell.value || cell.value === "") {
+        return null;
+      }
+      if (cell.value == deckId){
+        return sheet.getCellByA1(`${deckNameTable.endColumn}${i}`).value;
+      }
+    }
+    return null;
+  }
+
+  _saveNameOfDeck = async (sheet, deckNameTable, deckId, deckName) => { 
+    const idCell = await this._findFirstEmptyCellInColumn(sheet, deckNameTable.startColumn, deckNameTable.startRow, deckNameTable.endRow);
+    idCell.value = deckId;
+    const nameCell = sheet.getCellByA1(`${deckNameTable.endColumn}${idCell.a1Row}`);
+    nameCell.value = deckName;
+    await sheet.saveUpdatedCells();
+  }
+  
   _findFirstEmptyCellInColumn = async (sheet, columnLetter, startRow, endRow) => {
     for (let i = startRow; i <= endRow; i++) {
       const cell = sheet.getCellByA1(`${columnLetter}${i}`);
